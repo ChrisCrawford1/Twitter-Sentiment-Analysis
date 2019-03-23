be9112b52becfc602e3746618a15735dbdcfe495
Chris Crawford <scottishegg@live.co.uk>
2019-03-23 15:15:46 +0000

    Add subjectivity and sample size to dataframe

--- Main/TweetStatistics.py
+++ Main/TweetStatistics.py
@@ -1,5 +1,7 @@
 from textblob import TextBlob
 import pandas as pd
+import numpy as np
+import math
 
 class TweetStatistics:
 
@@ -8,15 +10,17 @@ class TweetStatistics:
         self.positive = 0
         self.neutral = 0
         self.negative = 0
+        self.subjectivity = 0
         self.user_input = user_input
         self.tweet_count = tweet_count
 
     def get_statistics(self):
         for tweet in self.tweets:
             blob = TextBlob(tweet.text)
+            self.subjectivity += blob.sentiment.subjectivity
             # print(tweet.created_at)
             self.rate_polarity(blob.sentiment.polarity)
-            
+
         self.print_output()
 
     def rate_polarity(self, polarity):
@@ -27,11 +31,18 @@ class TweetStatistics:
         elif polarity < 0.0:
             self.negative += 1
 
+    def calculate_subjectivity(self):
+        return round((self.subjectivity / self.tweet_count), 2)
+
+
     def print_output(self):
+        print("\nTweet analysis for the #{}\n".format(self.user_input))
         data_frame = pd.DataFrame({
             'Positive': self.positive,
             'Negative': self.negative,
-            'Neutral': self.neutral
+            'Neutral': self.neutral,
+            'Subjectivity': self.calculate_subjectivity(),
+            'Sample Size': self.tweet_count
         }, index = [self.user_input])
 
         print(data_frame)
